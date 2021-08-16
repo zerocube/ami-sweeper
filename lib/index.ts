1f2471ca20725ca5a4292fa4568ab27c2442cf05
Jordan Kueh <jordan@zerocu.be>
2021-08-16 17:18:51 +1000

    Initial commit - Bringing across source code from internal repository

---
+++ lib/index.ts
@@ -0,0 +1,60 @@
+import * as cdk from '@aws-cdk/core';
+
+import { Code, Function as LambdaFunction, Runtime } from '@aws-cdk/aws-lambda';
+
+import path = require('path');
+import { PolicyStatement } from '@aws-cdk/aws-iam';
+import { Rule, RuleTargetInput, Schedule } from '@aws-cdk/aws-events';
+import { LambdaFunction as LambdaFunctionEventsTarget } from '@aws-cdk/aws-events-targets';
+import { ImageTag } from './lambda/ami-sweeper';
+
+export interface AmiSweeperProps {
+  lambdaTimeout: cdk.Duration
+  lambdaRetries: number
+  schedule?: Schedule
+  imageTags: ImageTag[]
+}
+
+export class AmiSweeper extends cdk.Construct {
+
+  public readonly lambdaFunction: LambdaFunction
+  public readonly scheduledEventRule: Rule
+
+  constructor(scope: cdk.Construct, id: string, props: AmiSweeperProps = {
+    lambdaTimeout: cdk.Duration.minutes(1),
+    lambdaRetries: 2,
+    imageTags: [{ name: "deleteme", value: "true" }]
+  }) {
+    super(scope, id);
+
+    // Define construct contents here
+    this.lambdaFunction = new LambdaFunction(this, "SweeperLambda", {
+      runtime: Runtime.NODEJS_14_X,
+      handler: "index.handler",
+      code: Code.fromAsset(path.join(__dirname, 'lambda', 'ami-sweeper')),
+      retryAttempts: props.lambdaRetries,
+      timeout: props.lambdaTimeout,
+      initialPolicy: [
+        new PolicyStatement({
+          actions: [
+            "ec2:DescribeImages",
+            "ec2:DeregisterImage",
+            "ec2:DeleteSnapshot",
+            "autoscaling:DescribeAutoScalingGroups",
+            "imagebuilder:ListImages",
+            "imagebuilder:GetImage",
+          ],
+          resources: ["*"],
+        })
+      ],
+    });
+
+    new Rule(this, `SweeperScheduleRule`, {
+      // Documentation: https://docs.aws.amazon.com/lambda/latest/dg/services-cloudwatchevents-expressions.html
+      schedule: props.schedule,
+      targets: [new LambdaFunctionEventsTarget(this.lambdaFunction, {
+        event: RuleTargetInput.fromObject({ tags: props.imageTags })
+      })]
+    });
+  }
+}
---
+++ lib/lambda/ami-sweeper/index.ts
@@ -0,0 +1,137 @@
+'use strict';
+
+import EC2 = require('aws-sdk/clients/ec2');
+
+export interface ImageTag {
+  name: string
+  value: string
+}
+
+interface LambdaEvent {
+  imageTags: ImageTag[]
+  dryRun?: boolean
+  verbose?: boolean
+  deleteFirst?: boolean
+}
+
+async function processEvent(event: LambdaEvent) {
+
+  if (event.verbose) {
+    console.log("Received event:", JSON.stringify(event));
+  }
+  // We need to retrieve all AMIs.
+  var ec2 = new EC2();
+  let images = await ec2.describeImages({
+    Filters: event.imageTags.map((t) => {
+      return { Name: t.name, Values: [t.value] }
+    })
+  }).promise();
+
+  if (!images.Images) {
+    console.log("No images found. Skipping")
+    return
+  }
+
+  if (images.Images.length <= 1) {
+    console.log("One or less images present. Skipping.")
+    return
+  }
+
+  // Sort the images
+  images.Images.sort((a, b) => {
+    if (!a.CreationDate || !b.CreationDate) { return 0; }
+    let aDate = Date.parse(a.CreationDate);
+    let bDate = Date.parse(b.CreationDate)
+    // return aDate - bDate // Sort ascending
+    return bDate - aDate // Sort descending
+  });
+
+  if (event.verbose) {
+    console.log("Sorted images:")
+    images.Images.forEach((img) => {
+      console.log("-", img.CreationDate)
+    })
+    images.Images.forEach(async (image, index) => {
+      if (index == 0 && !event.deleteFirst) {
+        console.log("First image will be skipped:", image.ImageId)
+      } else {
+        console.log("This will be deleted:", image.ImageId)
+      }
+    })
+  }
+
+  for (let index = 0; index < images.Images.length; index++) {
+    let image = images.Images[index]
+    if (index == 0 && !event.deleteFirst) {
+      if (event.verbose) {
+        console.log("Skipping first image:", image.ImageId)
+      }
+      continue
+    }
+    if (!image.ImageId) {
+      console.log("Skipping image with no image ID")
+      continue
+    }
+    console.log("Found image (to be deleted):", image.ImageId, "-", image.Name);
+
+    // Do not proceed if dryRun has been set to 'true'
+    if (event.dryRun) {
+      console.log("Not proceeding with deletion as dryRun is 'true'.")
+      continue
+    }
+
+    // Get the snapshot ID(s) for thsi image
+    var snapshotIds: string[] = [];
+    if (image.BlockDeviceMappings) {
+      for (let index = 0; index < image.BlockDeviceMappings.length; index++) {
+
+        let mapping = image.BlockDeviceMappings[index]
+
+        if (mapping.Ebs && mapping.Ebs.SnapshotId) {
+          console.log("Will delete snapshot after the AMI:", mapping.Ebs.SnapshotId);
+          snapshotIds.push(mapping.Ebs.SnapshotId);
+        } else {
+          if (event.verbose) {
+            console.log("No EBS snapshot detected for mapping:", JSON.stringify(mapping))
+          }
+        }
+      }
+    }
+
+    // Delete the AMI
+    if (event.verbose) {
+      console.log("Attempting to deregister", image.ImageId)
+    }
+    let deregisterParams: EC2.DeregisterImageRequest = { ImageId: image.ImageId }
+    let deregisterResponse = await ec2.deregisterImage(deregisterParams).promise();
+    if (deregisterResponse.$response.httpResponse.statusCode != 200) {
+      console.log("Non-200 response code returned when trying to delete AMI:", image.ImageId)
+      continue
+    }
+    console.log("Deregistered AMI:", image.ImageId);
+
+    // Delete the snapshot(s)
+    for (let index = 0; index < snapshotIds.length; index++) {
+      let snapshotId = snapshotIds[index];
+      let snapshotDeleteParams: EC2.DeleteSnapshotRequest = { SnapshotId: snapshotId }
+      let snapshotDeleteResponse = await ec2.deleteSnapshot(snapshotDeleteParams).promise();
+      if (snapshotDeleteResponse.$response.httpResponse.statusCode != 200) {
+        console.warn("Non-200 response code returned when trying to delete snapshot:", snapshotId);
+        continue
+      }
+      console.log("Deleted snapshot:", snapshotId);
+    }
+  };
+}
+
+exports.handler = async (event: LambdaEvent, context: any): Promise<any> => {
+  await processEvent(event);
+};
+
+if (process.env.RUN_LOCALLY?.trim().toLowerCase() == "true") {
+  processEvent({
+    imageTags: [{ name: "deleteme", value: "true" }],
+    dryRun: true,
+    verbose: true,
+  });
+}
